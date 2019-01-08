d926dfd31076457bffb69c4589519da543b5c99d
ostn <9665466@qq.com>
2019-01-08 10:33:36 +0800

    Tips: 备注修改

--- server/common.js
+++ server/common.js
@@ -2,7 +2,7 @@
 export default {
     web_name:'微笑话管理系统',
     web_domain:'http://127.0.0.1:8080',	//访问域名
-    web_url:'http://127.0.0.1',	//访问域名
+    web_url:'http://127.0.0.1:80',	//部署时需要修改为真正域名
     nick_reg:/^[a-z\d\u4e00-\u9fa5]{1,8}$/, //昵称验证
     nick_txt:'最长不超过8个英文或中文字符', //昵称规则
 	name_reg:/^[a-zA-Z0-9_-]{4,10}$/i, //帐号验证
